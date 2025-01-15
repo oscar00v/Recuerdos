66ccb28688705dbaf65207665a692a3b9b32fbba
Oscar00v <Oscar00v@gmail.com>
2025-01-15 12:03:52 -0600

    Footer y redes sociales

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import logo from '../../assets/images/Logo Recuerdos Mariposa.png'; // Importa la imagen
 
+import "./Footer.scss"
 function Footer() {
   return (
     <div>
       <footer>
+        <div className='Logo'>
+          <img src={logo} alt="Logo Recuerdos Mariposa" />
+        </div>
+        <div className='BotonRedes'>
+          <button
+            type="button"
+            className="btn btn-primary btn-lg btn-floating mx-2"
+            
+          >
+            <i className="fab fa-facebook-f" />
+          </button>
+          <button
+            type="button"
+            className="btn btn-primary btn-lg btn-floating mx-2"
+            
+          >
+            <i className="fab fa-facebook-f" />
+          </button>
+          <button
+            type="button"
+            className="btn btn-primary btn-lg btn-floating mx-2"
+            
+          >
+            <i className="fab fa-facebook-f" />
+          </button>
+
+        </div>
         Footer
       </footer>
     </div>
